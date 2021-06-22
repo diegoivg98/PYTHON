236692ddb504c2cf54895359d6a9a7736fb6d31e
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-06-22 02:31:20 -0400

    Add files via upload

---
+++ ocdec.py
@@ -0,0 +1,17 @@
+#CONVERSOR OCTAL
+oc = '14'
+pos = len(oc) - 1
+nd = 0
+print ("NUMERO OCTAL:",oc)
+
+if oc == '9':
+	print("NO ES UN OCTAL")
+else:
+	for num in oc:
+		digito = int(num)
+		mult = digito * 2 ** pos
+		print(f'Dígito: {digito}, posición: {pos}, multiplicación: {mult}')
+		nd += int(num) * 8 ** pos
+		pos -= 1 
+
+print("NUMERO DECIMAL:",nd)
