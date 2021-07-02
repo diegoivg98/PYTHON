822f75ca59ea2149949d94fe9e552c882ded32b8
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-01 21:47:33 -0400

    Delete tabmult.py

--- EJERCICIOS/tabmult.py
+++
@@ -1,8 +0,0 @@
-#INGRESAR UN NUMERO Y OBTENER SU TABLA DE MULTIPLICAR DEL 1 AL 10
-num = int(input("INGRESA UN NUMERO: "))
-res = 0
-
-for x in range(1,11):
-	res = num * x
-	print(num,"X",x," = ",res)
-	x += 1
