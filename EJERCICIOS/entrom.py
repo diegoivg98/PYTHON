bb6ec5d6c610b79751d567f81b4e49e32905a950
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 02:20:52 -0400

    Update entrom.py

--- EJERCICIOS/entrom.py
+++ EJERCICIOS/entrom.py
@@ -2,13 +2,11 @@
 import sys
 lista_dec = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
 lista_rom = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
-
 while True:
 	print("")
 	num = int(input("INGRESE UN NUMERO DEL 1 AL 1000: "))
 	if num==0:
 		break
-
 	i=0
 	while num>0:
 		if num >= lista_dec[i]:
