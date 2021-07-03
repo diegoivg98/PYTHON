bbcb6fbf44f033835e79ad7fa46e986c4300aeaa
diegoivg98 <68931257+diegoivg98@users.noreply.github.com>
2021-07-03 02:19:24 -0400

    Update divcocres.py

--- EJERCICIOS/divcocres.py
+++ EJERCICIOS/divcocres.py
@@ -1,4 +1,4 @@
+#INGRESAR 2 NUMEROS, OBTENER EL COCIENTE Y RESTO DE LA DIVISION
 n1 = input("ingrese un numero: ")
 n2 = input("ingrese otro numero: ")
-
 print("la division entre",n1," y ",n2," da un cociente de ",str(int(n1)//int(n2))," y un resto de ",str(int(n1) % int(n2)))
