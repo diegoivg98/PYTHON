d9480ba61bd6963346368921ea579b7949f4dd8f
Diego Vistoso <68931257+diegoivg98@users.noreply.github.com>
2022-08-24 23:44:26 -0400

    Add files via upload
    tipos de datos, listas y diccionarios

---
+++ EJERCICIOS/python-obc/ej5.py
@@ -0,0 +1,21 @@
+nombre: str = 'Diego'
+apellidos: str = 'Vistoso'
+edad: int = 23
+email: str = 'email@gmail.com' 
+telefono: int = 987654321
+casado: bool = False
+con_hijos: bool = False
+lista_de_amigos: list = ['Octavio','Nicolas','Cesar']
+peliculas_vistas: dict = {
+    "p1":"SAW",
+    "p2":"Silent Hill"}
+
+print(nombre)
+print(apellidos)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(con_hijos)
+print(lista_de_amigos)
+print(peliculas_vistas)
