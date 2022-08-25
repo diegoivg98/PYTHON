29b97bd21601918b5d8cc7b1f4023fe50b3f3f7d
Diego Vistoso <68931257+diegoivg98@users.noreply.github.com>
2022-08-25 17:20:33 -0400

    Add files via upload
    funcion para calcular area triangulo y circulo

---
+++ EJERCICIOS/python-obc/ej10.py
@@ -0,0 +1,10 @@
+import math
+def area_triangulo(altura,base):
+    print(float(base * altura) / 2)
+
+area_triangulo(5,10)
+
+def area_circulo(r):
+    print(float(math.pi * (r**2)))
+
+area_circulo(3)
