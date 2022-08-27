6b0e1828691d50959bc1e1aa63f6d5c1b35f5f84
Diego Vistoso <68931257+diegoivg98@users.noreply.github.com>
2022-08-26 21:00:46 -0400

    Add files via upload
    creacion modulo en base a calculadora

---
+++ EJERCICIOS/python-obc/ej15/resultados.py
@@ -0,0 +1,10 @@
+# Importing all the functions from the file operaciones.py
+from operaciones import * 
+
+a = 2
+b = 3
+
+print(suma(a,b))
+print(resta(a,b))
+print(multiplicar(a,b))
+print(dividir(a,b))
